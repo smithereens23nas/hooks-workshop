7b21a990ca63791fdd0886da20efcf43929b76d9
smiethereens23nas <nicholas23smith@gmail.com>
2022-11-18 13:38:11 -0500

    started hook training

--- lessons/01-rendering/exercise/LoggedOut.final.js
+++ lessons/01-rendering/exercise/LoggedOut.final.js
@@ -15,7 +15,7 @@ export default function LoggedOut() {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <LoginForm />
+            <LoginForm /> 
           </TabPanel>
           <TabPanel>
             <SignupForm />
